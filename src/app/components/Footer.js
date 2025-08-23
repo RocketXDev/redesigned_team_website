e948a691ff8c5a98fc99e21cacd17d8f1b8157a7
RocketXDev <amarkelousa@gmail.com>
2025-08-23 08:32:55 -0600

    Initial Deployment. Home page created

---
+++ src/app/components/Footer.js
@@ -0,0 +1,12 @@
+import '../components.css'
+
+export default function Footer() {
+    return (
+        <div>
+            <div className="footer-wrapper">
+                <div className="footer-title">Leah Neset & Artem Markelov</div>
+                <div className="footer-copyright">Copyright © 2023</div>
+            </div>
+        </div>
+    )
+}
---
+++ src/app/components/Nav.js
@@ -0,0 +1,19 @@
+import '../components.css'
+
+export default function Nav() {
+    return (
+        <div>
+            <div className="nav-wrapper">
+                <div className="nav-title">Leah Neset & Artem Markelov</div>
+                <ul className="nav-links">
+                    <li>About Us</li>
+                    <li>Training</li>
+                    <li>Competitions</li>
+                    <li>Videos</li>
+                    <li>Social Media</li>
+                    <li>Sponsors</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
