e157363277c282e8fc36c306c75954f985f394a9
Artem Markelov <markelovartemka@icloud.com>
2026-03-15 09:03:02 -0500

    Added link to order hoodies

--- src/app/pages/store/page.js
+++ src/app/pages/store/page.js
@@ -1,6 +1,8 @@
+'use client'
 import Nav from '../../components/Nav';
 import Footer from '../../components/Footer';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Store() {
   return (
@@ -11,17 +13,20 @@ export default function Store() {
                 <div className='store-product'>
                     <Image alt='NM Hoodie' width={500} height={300} src='/merch_design.png' className='pr-img'></Image>
                     <div className='pr-name'>NM Hoodie</div>
-                    <div className='pr-price'>$35.00 USD</div>
+                    <div className='pr-price'>$40.00 USD</div>
+                    <div className="pr-order"><a href="https://docs.google.com/forms/d/e/1FAIpQLSd9UzgIyPMst0uqA4h1nfFslhVnPTO2gcch5C6yk-HAnnLPPA/viewform?usp=header">Order</a></div>
                 </div>
                 <div className='store-product'>
-                    <Image alt='NM Keychains' width={300} height={280} src='/pins_logo.png' className='pr-img'></Image>
+                    <Image alt='NM Keychains' width={330} height={330} src='/keychain.png' className='pr-img'></Image>
                     <div className='pr-name'>NM Keychain</div>
                     <div className='pr-price'>$8.00 USD</div>
+                    <div className="pr-order"><a href="#">Out of Stock</a></div>
                 </div>
                 <div className='store-product'>
-                    <Image alt='NM Pins' width={300} height={280} src='/pins_logo.png' className='pr-img'></Image>
+                    <Image alt='NM Pins' width={330} height={330} src='/pin.png' className='pr-img pins'></Image>
                     <div className='pr-name'>NM Pin</div>
                     <div className='pr-price'>$6.00 USD</div>
+                    <div className="pr-order"><a href="#">Out of Stock</a></div>
                 </div>
             </div>
         </div>
